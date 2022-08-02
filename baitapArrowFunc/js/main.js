6a45a898b0f0250e7bbb1894cb2563dc7bbeb2ab
khoa lnt <khoa201191@gmail.com>
2022-08-03 02:41:07 +0700

    complete ex

--- baitapArrowFunc/js/main.js
+++ baitapArrowFunc/js/main.js
@@ -13,16 +13,25 @@ const colorList = [
 ];
 
 const getElement = (id) => document.getElementById(id);
-const querySelect = (i) => document.querySelector(i);
 window.onload = () => {
   let html = [];
   for (let i = 0; i < colorList.length; i++) {
-    html += `<button class="color-button ${colorList[i]}" onclick="changeColor('${colorList[i]}')"></button>`;
+    html += `<button class="color-button ${colorList[i]}" onclick="changeActive('${colorList[i]}')"></button>`;
   }
-  btnChangeColor = document.getElementById("colorContainer").innerHTML = html;
+  document.getElementById("colorContainer").innerHTML = html;
 };
 
-const changeColor = (color) => {
-  getElement("house").className = `house ${color}`;
-  getElement("house").style.backgroundColor = `${color}`;
+const changeActive = (color) => {
+  const btn = document.querySelectorAll(`#colorContainer button`);
+  for (let i = 0; i < btn.length; i++) {
+    const val = btn[i].classList;
+    console.log("val: ", val);
+    if (val.value === `color-button ${color}`) {
+      btn[i].classList.add("active");
+      getElement("house").classList = `house ${color}`;
+      getElement("house").style.backgroundColor = `${color}`;
+    } else {
+      btn[i].classList.remove("active");
+    }
+  }
 };
